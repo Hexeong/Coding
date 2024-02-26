b2e82544b1236a5f118e78133c6956a236cfc30d
HEX <123macanic@naver.com>
2024-02-27 00:47:17 +0900

    solving 15654.cpp

---
+++ Backjoon/15654.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> res;
+
+void tracking(int n) {
+	if (n == M) {
+		for (int i = 0; i < M; i++) cout << res[i] << " ";
+		cout << "\n";
+		return;
+	}
+	else {
+		int temp;
+		for (int i = 0; i < N; i++) {
+			temp = arr[i];
+
+			n++;
+			res.push_back(temp);
+			arr.erase(arr.begin() + i);
+			tracking(n);
+			arr.insert(arr.begin() + i, temp);
+			res.pop_back();
+			n--;
+		}
+		return;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int temp; cin >> temp;
+		arr.push_back(temp);
+	}
+
+	sort(arr.begin(), arr.begin() + N);
+
+	tracking(0);
+
+	return 0;
+}
