683358d2b9225c979230e750835db88a87e49a27
Hex <123macanic@naver.com>
2024-02-28 17:23:56 +0900

    solving 14499.cpp

--- Backjoon/14499.cpp
+++ Backjoon/14499.cpp
@@ -1,29 +1,81 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int N, M, x, y, K;
+int order[1000];
+int map[20][20];
+int bottom = 6;
 
-int dice[6][2][2] = {
-	{ // 1
-		{5, 2}, {4, 3} // ver, hor
-	},
-	{ // 2
-		{1, 6}, {4, 3}
-	},
-	{ // 3
-		{5, 2}, {1, 4}
-	},
-	{ // 4
-		{5, 2}, {3, 1}
-	},
-	{ // 5
-		{6, 1}, {4, 3}
-	},
-	{ // 6
-		{2, 5}, {4, 3}
-	},
-};
+int dice[6] = { 1, 2, 3, 4, 5, 6 };
+// 1, 3, 4, 6 - 동 서
+// 1, 2, 5, 6 - 북 남
+
+bool check(int o) {
+	if (o == 1) {
+		if (x == N - 1) return true;
+		else return false;
+	}
+	else if (o == 2) {
+		if (x == 0) return true;
+		else return false;
+	}
+	else if (o == 3) {
+		if (y == 0) return true;
+		else return false;
+	}
+	else {
+		if (y == M - 1) return true;
+		else return false;
+	}
+}
+
+void roll(int o) {
+	if (o == 1) {
+		x++;
+		int temp = dice[0];
+		dice[0] = dice[2];
+		dice[2] = dice[3];
+		dice[3] = dice[5];
+		dice[5] = temp;
+	}
+	else if (o == 2) {
+		x--;
+		int temp = dice[0];
+		dice[0] = dice[5];
+		dice[5] = dice[3];
+		dice[3] = dice[2];
+		dice[2] = temp;
+	}
+	else if (o == 3) {
+		y--;
+		int temp = dice[0];
+		dice[0] = dice[5];
+		dice[5] = dice[4];
+		dice[4] = dice[1];
+		dice[1] = temp;
+	}
+	else {
+		y++;
+		int temp = dice[0];
+		dice[0] = dice[1];
+		dice[1] = dice[4];
+		dice[4] = dice[5];
+		dice[5] = temp;
+	}
+}
+
+void revert() {
+	if (map[x][y] == 0) {
+		// bottom == dice[5]
+		map[x][y] = dice[5];
+	}
+	else {
+		dice[5] = map[x][y];
+		map[x][y] = 0;
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -31,6 +83,22 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> N >> M >> x >> y >> K;
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) {
+		cin >> map[i][j];
+	}
+
+	for (int i = 0; i < K; i++) {
+		cin >> order[i];
+		if (check(order[i])) { // 밖으로 넘어가는지 check
+			continue;
+		}
+		// 주사위 굴리기
+		roll(order[i]);
+		// 값 변환
+		revert();
+		// 값 출력
+		cout << dice[0] << "\n";
+	}
 
 
 
