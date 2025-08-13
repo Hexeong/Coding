47fae3f2d4e5558e3e4aa9838c06cd77fe262b72
HEX <123macanic@naver.com>
2025-08-14 00:15:50 +0900

    feat: BOJ 6549.cpp solved

---
+++ Baekjoon_C++/6549.cpp
@@ -0,0 +1,14 @@
+//
+// Created by admin on 2025-08-12.
+// https://www.acmicpc.net/problem/6549
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long result = 0;
+}
