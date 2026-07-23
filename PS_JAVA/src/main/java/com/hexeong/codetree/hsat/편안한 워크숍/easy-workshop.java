b090af9d55a994aec278dd69f168fcde1649af30
in seong Park <74069492+Hexeong@users.noreply.github.com>
2026-07-23 22:07:56 +0900

    [Passed] 편안한 워크숍 (179ms, 12MB)

--- PS_JAVA/src/main/java/com/hexeong/codetree/hsat/편안한 워크숍/easy-workshop.java
+++ PS_JAVA/src/main/java/com/hexeong/codetree/hsat/편안한 워크숍/easy-workshop.java
@@ -1,68 +1,69 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
 
-    static int[][][] mem;
-
-    static int n = 0, k = 0, min = Integer.MAX_VALUE;
+    static int N, K;
     static int[][] grid;
+    static int[][][][] mem; 
+    // mem[i][j][d][k] = 인접한 높이의 차들 간 최댓값
+    // i = 현재 행 idx
+    // j = 현재 열 idx
+    // d = 현재 위치로 넘어온 방향
+    // k = 현재까지 등산로의 길이
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        grid = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                grid[i][j] = sc.nextInt();
 
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        mem = new int[N][N][4][K + 1];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                for (int d = 0; d < 4; d++) 
+                    Arrays.fill(mem[i][j][d], -1);
 
-        grid = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < n; j++) 
-                grid[i][j] = Integer.parseInt(st.nextToken());
-        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                for (int d = 0; d < 4; d++)
+                    min = Math.min(min, dfs(i, j, d, 0));
 
-        mem = new int[n][n][k + 1];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                Arrays.fill(mem[i][j], -1);
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                min = Math.min(min, dfs(i, j, 1));
-            }
-        
-        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+        if (min == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(min);
     }
 
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dy = {1, 0 ,-1, 0};
     static int[] dx = {0, 1, 0, -1};
 
-    static int dfs(int cy, int cx, int len) {
-        if (len == k) {
-            return 0;
-        }
+    static int dfs(int cy, int cx, int cd, int ck) {
+        if (ck == K - 1) return -1;
+        if (mem[cy][cx][cd][ck] != -1) return mem[cy][cx][cd][ck];
 
-        if (mem[cy][cx][len] != -1) {
-            return mem[cy][cx][len];
-        }
-
-        int cmin = Integer.MAX_VALUE;
+        int cnt = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
+            if ((cd + 2) % 4 == i) continue;
+
             int ny = cy + dy[i];
             int nx = cx + dx[i];
 
-            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-            if (grid[cy][cx] >= grid[ny][nx]) continue;
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+            if (grid[ny][nx] <= grid[cy][cx]) continue;
 
-            int diff = grid[ny][nx] - grid[cy][cx];
+            int res = dfs(ny, nx, i, ck + 1);
+            if (res == Integer.MAX_VALUE) continue;
 
-            int maxDiffOnThisPath = Math.max(diff, dfs(ny, nx, len + 1));
-            cmin = Math.min(cmin, maxDiffOnThisPath);
+            res = Math.max(grid[ny][nx] - grid[cy][cx], res);
+            cnt = Math.min(cnt, res);
         }
-        return mem[cy][cx][len] = cmin;
+
+        return mem[cy][cx][cd][ck] = cnt;
     }
 }
-
