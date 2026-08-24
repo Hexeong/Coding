3eaef8547c714c5216d45fa93276da101399da95
in seong Park <74069492+Hexeong@users.noreply.github.com>
2026-08-24 15:39:46 +0900

    [Passed] 연속 부분 합의 최댓값 구하기 2 (389ms, 23MB)

---
+++ PS_JAVA/src/main/java/com/hexeong/codetree/trail5/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
